0f74e8fc01d67fb73b464cf244f13c5eeb20a442
xiaomoL444 <2802273114@qq.com>
2023-10-20 20:57:57 +0800

    为HttpClass添加注释

--- MysBotSDK/Tool/HttpClass.cs
+++ MysBotSDK/Tool/HttpClass.cs
@@ -5,49 +5,67 @@ namespace MysBotSDK.Tool;
 
 public static class HttpClass
 {
-    private static HttpClientHandler clientHandle;
-    private static HttpClient client;
-    static HttpClass()
-    {
-        clientHandle = new HttpClientHandler()
-        {
-            UseCookies = true,
-            AutomaticDecompression = DecompressionMethods.GZip,
-            UseProxy = false,
-            Proxy = null,
-            UseDefaultCredentials = true,
-            AllowAutoRedirect = true,
-            ClientCertificateOptions = ClientCertificateOption.Automatic,
-            ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; },
-        };
-        client = new HttpClient(clientHandle) { Timeout = new TimeSpan(0, 1, 0) };
+	private static HttpClientHandler clientHandle;
+	private static HttpClient client;
 
-        CacheControlHeaderValue cacheControl = new CacheControlHeaderValue();
-        cacheControl.NoCache = true;
-        cacheControl.NoStore = true;
-        client.DefaultRequestHeaders.CacheControl = cacheControl;
-    }
-    public static async Task<HttpResponseMessage> SendAsync(HttpRequestMessage httpRequestMessage)
-    {
-        var res = await client.SendAsync(httpRequestMessage, HttpCompletionOption.ResponseHeadersRead);
-        return res;
-    }
-    public static HttpRequestMessage AddHeaders(this HttpRequestMessage httpRequestMessage, string cookies)
-    {
-        StringReader stringReader = new StringReader(cookies);
-        string? temp_str;
-        while ((temp_str = stringReader.ReadLine()) != null)
-        {
-            if (temp_str.EndsWith(";"))
-            {
-                temp_str = temp_str.Substring(0, temp_str.Length - 1);
-            }
-            int index = temp_str.IndexOf(':');
-            string name = temp_str.Substring(0, index);
-            string value = temp_str.Substring(index + 1, temp_str.Length - 1 - index);
-            //httpRequestMessage.Headers.Add(name,value);
-            httpRequestMessage.Headers.TryAddWithoutValidation(name, value);
-        }
-        return httpRequestMessage;
-    }
+	/// <summary>
+	/// 静态构造器
+	/// </summary>
+	static HttpClass()
+	{
+		clientHandle = new HttpClientHandler()
+		{
+			UseCookies = true,
+			AutomaticDecompression = DecompressionMethods.GZip,
+			UseProxy = false,
+			Proxy = null,
+			UseDefaultCredentials = true,
+			AllowAutoRedirect = true,
+			ClientCertificateOptions = ClientCertificateOption.Automatic,
+			ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; },
+		};
+		client = new HttpClient(clientHandle) { Timeout = new TimeSpan(0, 1, 0) };
+
+		CacheControlHeaderValue cacheControl = new CacheControlHeaderValue();
+		cacheControl.NoCache = true;
+		cacheControl.NoStore = true;
+		client.DefaultRequestHeaders.CacheControl = cacheControl;
+	}
+
+	/// <summary>
+	/// 异步发送Http协议消息
+	/// </summary>
+	/// <param name="httpRequestMessage">请求消息</param>
+	/// <returns></returns>
+	public static async Task<HttpResponseMessage> SendAsync(HttpRequestMessage httpRequestMessage)
+	{
+		var res = await client.SendAsync(httpRequestMessage, HttpCompletionOption.ResponseHeadersRead);
+		return res;
+	}
+
+	/// <summary>
+	/// 为请求信息添加请求头
+	/// </summary>
+	/// <param name="httpRequestMessage">请求信息</param>
+	/// <param name="cookies">请求头</param>
+	/// <returns></returns>
+	public static HttpRequestMessage AddHeaders(this HttpRequestMessage httpRequestMessage, string cookies)
+	{
+		StringReader stringReader = new StringReader(cookies);
+		string? temp_str;
+
+		while ((temp_str = stringReader.ReadLine()) != null)//分割请求头，以第一个:为分割符
+		{
+			if (temp_str.EndsWith(";"))
+			{
+				temp_str = temp_str.Substring(0, temp_str.Length - 1);
+			}
+			int index = temp_str.IndexOf(':');
+			string name = temp_str.Substring(0, index);
+			string value = temp_str.Substring(index + 1, temp_str.Length - 1 - index);
+			//httpRequestMessage.Headers.Add(name,value);
+			httpRequestMessage.Headers.TryAddWithoutValidation(name, value);
+		}
+		return httpRequestMessage;
+	}
 }
