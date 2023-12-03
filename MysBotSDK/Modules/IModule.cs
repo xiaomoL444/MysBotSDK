0cf1ad551b4981c77dd0a34e6be90bbda78d1ec7
xiaomoL444 <2802273114@qq.com>
2023-12-03 17:42:41 +0800

    准备将SDK支持以Main程序启动

---
+++ MysBotSDK/Modules/IModule.cs
@@ -0,0 +1,43 @@
+﻿using MysBotSDK.MessageHandle;
+
+namespace MysBotSDK.Modules;
+
+/// <summary>
+/// 接收器事件接口
+/// </summary>
+public interface IMysReceiverModule
+{
+	/// <summary>
+	/// 触发器
+	/// </summary>
+	/// <param name="base">传递的接收器参数</param>
+	/// <returns></returns>
+	public Task Execute(MessageReceiverBase @base);
+
+	/// <summary>
+	/// 是否启用插件
+	/// </summary>
+	public bool? isEnable { get; set; }
+}
+
+/// <summary>
+/// 多线程启动接口
+/// </summary>
+public interface IMysTaskModule
+{
+	/// <summary>
+	/// 程序启动时执行的命令
+	/// </summary>
+	/// <returns></returns>
+	public Task Start();
+
+	/// <summary>
+	/// 程序卸载插件时执行的命令
+	/// </summary>
+	public void UnLoad();
+
+	/// <summary>
+	/// 是否启用插件
+	/// </summary>
+	public bool? isEnable { get; set; }
+}
---
+++ MysBotSDK/Modules/ReceiverAttributes.cs
@@ -0,0 +1,68 @@
+﻿namespace MysBotSDK.Modules;
+
+#region ExtenData
+/// <summary>
+/// 接收器特征
+/// </summary>
+public abstract class ExtendDataAttribute : Attribute { }
+
+/// <summary>
+/// 有用户加入大别野的接收器特征
+/// </summary>
+public class JoinVillaAttribute : ExtendDataAttribute { }
+
+/// <summary>
+/// 有用户发送@at消息的接收器特征
+/// </summary>
+public class SendMessageAttribute : ExtendDataAttribute
+{
+	/// <summary>
+	/// 唤起的命令
+	/// </summary>
+	public string Commond { get; set; }
+	/// <summary>
+	/// 触发该方法的命令，如命令"/Test"，则填入"Test"或者"、Test"，发送消息时可用"@bot /Test"或者"@bot Test"唤起
+	/// </summary>
+	/// <param name="commond"></param>
+	public SendMessageAttribute(string commond)
+	{
+		if (commond[0] == '/')
+		{
+			Commond = commond.Substring(1);
+		}
+		Commond = commond;
+	}
+}
+
+/// <summary>
+/// Bot加入大别野的接收器特征
+/// </summary>
+public class CreateRobotAttribute : ExtendDataAttribute { }
+
+/// <summary>
+/// Bot被移出大别野的接收器特征
+/// </summary>
+public class DeleteRobotAttribute : ExtendDataAttribute { }
+
+/// <summary>
+/// 用户发送表情回复消息接收器特征
+/// </summary>
+public class AddQuickEmoticonAttribute : ExtendDataAttribute { }
+
+/// <summary>
+/// 审核事件的接收器特征
+/// </summary>
+public class AuditCallbackAttribute : ExtendDataAttribute { }
+
+/// <summary>
+/// 面板点击事件的接收器特征
+/// </summary>
+public class ClickMsgComponentAttribute : ExtendDataAttribute { }
+#endregion
+
+#region Program
+
+public abstract class ProgramAttribute : Attribute { }
+
+public class StartAttribute : ProgramAttribute { }
+#endregion
